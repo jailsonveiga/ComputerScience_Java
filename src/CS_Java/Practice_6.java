e025cc309ad4739372fb65df335655f159dab8f2
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-01-27 21:25:36 -0500

    Practice_5

---
+++ src/CS_Java/Practice_6.java
@@ -0,0 +1,12 @@
+package CS_Java;
+
+public class Practice_6 {
+    public static void main(String[] args) {
+
+        int num = 3;
+
+        for(int i = 0; i < num; i++) {
+            System.out.println("# " + i + " - Meow!!");
+        }
+    }
+}
