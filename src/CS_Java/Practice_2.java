e0bc3ac55b2141bbb8fc0452a10477c13180ba61
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-01-27 19:17:07 -0500

    Practice_2

---
+++ src/CS_Java/Practice_2.java
@@ -0,0 +1,15 @@
+package CS_Java;
+
+import java.util.Scanner;
+
+public class Practice_2 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("What's your name?");
+        String name = input.nextLine();
+
+        System.out.println("Hello, " + name);
+    }
+}
