29bc7a1f48787fb707b3c7a707bd578d8a7f1b67
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-01-27 21:46:01 -0500

    Practice_6 Version_1

---
+++ src/CS_Java/Practice_6_Version_1.java
@@ -0,0 +1,17 @@
+package CS_Java;
+
+public class Practice_6_Version_1 {
+
+
+    public static void main(String[] args) {
+
+            meow(3);
+
+    }
+    public static void meow(int n) {
+
+        for(int i = 0; i < n; i++) {
+            System.out.println("meow!");
+        }
+    }
+}
