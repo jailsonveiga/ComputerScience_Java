3abeffc45201745008444db14fa11d4edbaa6ac6
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-01-29 19:27:26 -0500

    Practice_7_Version_1

--- src/CS_Java/Practice_7_Version_1.java
+++ src/CS_Java/Practice_7_Version_1.java
@@ -1,7 +1,28 @@
 package CS_Java;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Practice_7_Version_1 {
+
+    public static Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) {
+
+        System.out.println("Regular price at 15% discount");
+        float regular = input.nextFloat();
+
+        float sale = discount(regular);
+
+        DecimalFormat dis = new DecimalFormat("#.00");
+
+        System.out.println("The total price are: $ " + dis.format(sale) + " dollars");
+
+    }
+
+    public static float discount(float price) {
+
+        return price * .85f;
 
     }
 }
