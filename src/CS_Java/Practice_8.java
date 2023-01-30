0cd404381c48499c2ca70ed4cfa5bc1a1b2d7239
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-01-29 19:27:11 -0500

    Practice_8

---
+++ src/CS_Java/Practice_8.java
@@ -0,0 +1,9 @@
+package CS_Java;
+
+public class Practice_8 {
+    public static void main(String[] args){
+        for(int i = 0; i < 4; i++) {
+            System.out.print("?");
+        }
+    }
+}
