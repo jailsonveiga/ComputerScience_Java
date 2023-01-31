e45db4978f9045ef3dd2a1ca749db4c863cf452a
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-01-31 12:03:04 -0500

    Practice_11

---
+++ src/CS_Java/Practice_11.java
@@ -0,0 +1,9 @@
+package CS_Java;
+
+public class Practice_11 {
+    public static void main(String[] args) {
+        for(int i = 0; i < 3 ; i++) {
+            System.out.println("#");
+        }
+    }
+}
