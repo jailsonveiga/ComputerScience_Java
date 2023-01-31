796a7a921ccef87cc6d63309049e6274fff764a7
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-01-30 20:22:57 -0500

    Practice_10

--- src/CS_Java/Practice_10.java
+++ src/CS_Java/Practice_10.java
@@ -1,7 +1,19 @@
 package CS_Java;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Practice_10 {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Dollar amount");
+        float pennies = input.nextFloat();
+
+        int result =  Math.round(pennies * 100);
+
+        System.out.println("Pennies: " + result);
 
     }
 }
