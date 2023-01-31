543d48feb9557f87812a35ba7905e9a7d9d58ce4
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-01-31 12:38:53 -0500

    Practice_12 Version_1

---
+++ src/CS_Java/Practice_12_Version_1.java
@@ -0,0 +1,15 @@
+package CS_Java;
+
+public class Practice_12_Version_1 {
+    public static void main(String[] args) {
+
+        int[] score = new int[3];
+
+        score[0] = 72;
+        score[1] = 73;
+        score[2] = 33;
+
+        System.out.println((float)(score[0] + score[1] + score[2]) / 3);
+
+    }
+}
