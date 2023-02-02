db8ba984c4576f448b597345831961572c8e2c30
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-02-01 21:23:05 -0500

    Practice_14

---
+++ src/CS_Java/Practice_14.java
@@ -0,0 +1,29 @@
+package CS_Java;
+
+import java.util.Scanner;
+
+public class Practice_14 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your name?");
+        String name = input.nextLine();
+
+        int length = string_length(name);
+
+        System.out.println(length);
+    }
+
+    private static int string_length(String name) {
+
+        int i = 0;
+
+        while (i < name.length() && name.charAt(i) != '\0') {
+
+            i++;
+
+        }
+        return i;
+    }
+}
