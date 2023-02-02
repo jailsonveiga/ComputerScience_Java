bc6afa9254037ceab5008a704b800a8a106e7196
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-02-01 20:09:50 -0500

    Practice_13

--- src/CS_Java/Practice_13.java
+++ src/CS_Java/Practice_13.java
@@ -3,5 +3,19 @@ package CS_Java;
 public class Practice_13 {
     public static void main(String[] args) {
 
+        char c1 = 'H';
+        char c2 = 'I';
+        char c3 = '!';
+
+        System.out.println(""+ c1 + c2 + c3);
+
+        System.out.println((int) c1);
+        System.out.println((int) c2);
+        System.out.println((int) c3);
+
+
+        System.out.println((int) c1 + c2 + c3);
+
+
     }
 }
