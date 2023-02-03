2367a8aabaa290dff4a51b991ef9609f4215607e
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-02-03 10:53:43 -0500

    Command Line Arguments

--- src/Core_java/ComLiArg.java
+++ src/Core_java/ComLiArg.java
@@ -3,5 +3,11 @@ package Core_java;
 public class ComLiArg {
     public static void main(String[] args) {
 
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+
+        int addSum = num1 + num2;
+
+        System.out.println("The total of 2 numbers add are: " + addSum);
     }
 }
