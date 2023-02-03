419766112ba1cc2671c9f1c87a024d73e4b6c7f4
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-02-02 21:47:19 -0500

    Practice_16

---
+++ src/CS_Java/Practice_16.java
@@ -0,0 +1,11 @@
+package CS_Java;
+
+public class Practice_16 {
+    public static void main(String[] args) {
+
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+
+    }
+}
