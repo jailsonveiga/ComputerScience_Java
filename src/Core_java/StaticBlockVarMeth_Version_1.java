d758d7b2a51fcc239c1bc254521e0efa33e0372f
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-02-03 19:54:49 -0500

    StaticBlockVarMeth_Version_1 Access from other class Static Variable, Method and Static Block

---
+++ src/Core_java/StaticBlockVarMeth_Version_1.java
@@ -0,0 +1,18 @@
+package Core_java;
+
+public class StaticBlockVarMeth_Version_1 {
+    public static String name = "Jay";
+
+    public static void staticMethod() {
+
+        System.out.println("Static Method");
+
+    }
+
+    static {
+
+        System.out.println("Static Block");
+
+    }
+
+}
