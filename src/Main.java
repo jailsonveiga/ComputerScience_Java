735e3364ead86a8de51015aa55a742ec59e2c13f
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-02-04 18:53:05 -0500

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import Core_java.ConsOver;
 import Core_java.StaticBlockVarMeth_Version_1;
 
 public class Main {
@@ -7,6 +8,13 @@ public class Main {
       System.out.println(StaticBlockVarMeth_Version_1.name);
 
       StaticBlockVarMeth_Version_1.staticMethod();
+
+      System.out.println("--------------------");
+
+      ConsOver obj = new ConsOver();
+
+      ConsOver objs = new ConsOver("Hello");
+
 
     }
 }
