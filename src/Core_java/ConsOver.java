e204ba9131aca831235bae65f90ca815892f11c3
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-02-04 18:52:53 -0500

    Default Constructor, and Constructor With Parameters

---
+++ src/Core_java/ConsOver.java
@@ -0,0 +1,18 @@
+package Core_java;
+
+public class ConsOver {
+
+        public ConsOver() {
+
+            System.out.println("Constructor Default");
+
+        }
+
+        public ConsOver(String name) {
+
+            System.out.println("Constructor With Arguments");
+
+        }
+    }
+
+
