6c7eaff13e1fc24b1da4149a24b48b73e0e2bb10
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-02-03 19:53:28 -0500

    Main Update

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
+import Core_java.StaticBlockVarMeth_Version_1;
+
 public class Main {
 
     public static void main(String[] args) {
+
+      System.out.println(StaticBlockVarMeth_Version_1.name);
+
+      StaticBlockVarMeth_Version_1.staticMethod();
 
     }
 }
