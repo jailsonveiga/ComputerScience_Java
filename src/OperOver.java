a3edc777249348ca956ed379b83fc185b0fb7ec1
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-02-04 19:12:56 -0500

    Update

--- src/OperOver.java
+++ src/OperOver.java
@@ -4,9 +4,8 @@ public class OperOver {
 
         int num1 = 10;
         int num2 = 21;
-        int total = num1 + num2;
 
-        System.out.println("The Sum of two numbers are: " + total);
+        System.out.println("The Sum of two numbers are: " + (num1 + num2));
 
     }
 }
