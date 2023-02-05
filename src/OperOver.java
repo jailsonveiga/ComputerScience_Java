4a263d6dcf0169adc3fdcca83767ebaf3dce04a3
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-02-04 19:11:38 -0500

    Operator Overloading

---
+++ src/OperOver.java
@@ -0,0 +1,12 @@
+public class OperOver {
+
+    void add() {
+
+        int num1 = 10;
+        int num2 = 21;
+        int total = num1 + num2;
+
+        System.out.println("The Sum of two numbers are: " + total);
+
+    }
+}
