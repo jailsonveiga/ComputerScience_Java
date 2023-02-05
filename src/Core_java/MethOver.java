528b6439c96a92c36228678bb5a62b7de937b97c
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-02-04 19:04:34 -0500

    Method Overloading

---
+++ src/Core_java/MethOver.java
@@ -0,0 +1,17 @@
+package Core_java;
+
+public class MethOver {
+
+    public void add() {
+
+        System.out.println("Method Overloading");
+
+    }
+
+    public void add(int a, int b) {
+
+       int z = a + b;
+        System.out.println("Sum = " + z);
+
+    }
+}
