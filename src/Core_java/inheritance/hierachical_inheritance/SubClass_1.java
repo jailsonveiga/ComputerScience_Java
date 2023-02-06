cd1ad00863701f26efb8ecbca84a34be8b6ca484
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-02-06 12:09:19 -0500

    Hierarchical Inheritance

---
+++ src/Core_java/inheritance/hierachical_inheritance/SubClass_1.java
@@ -0,0 +1,13 @@
+package Core_java.inheritance.hierachical_inheritance;
+
+public class SubClass_1 extends SuperClass{
+
+    String color = "Blue";
+    int speed = 6;
+
+    void displaySubClass_1() {
+
+        System.out.println("Color " + color + " and speed is " + speed);
+
+    }
+}
