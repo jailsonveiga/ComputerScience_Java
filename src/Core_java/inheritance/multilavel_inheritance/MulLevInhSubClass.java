dc21efa7309832e85c702a7f8355824fea5fc0dc
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-02-06 11:51:45 -0500

    Multi Level Inheritance

---
+++ src/Core_java/inheritance/multilavel_inheritance/MulLevInhSubClass.java
@@ -0,0 +1,13 @@
+package Core_java.inheritance.multilavel_inheritance;
+
+public class MulLevInhSubClass extends MulLevInh{
+
+    String country = "Cape Verde";
+    String speak = "Criolo";
+
+    void displayMulLevInhSubClass() {
+
+        System.out.println("I'm from " + country + " and I speak " + speak);
+
+    }
+}
