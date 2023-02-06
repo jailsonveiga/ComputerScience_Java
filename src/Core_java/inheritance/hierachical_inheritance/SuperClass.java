f93af624fd68d6a07078a08a0d6094ed0b4eb47b
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-02-06 12:09:06 -0500

    Hierarchical Inheritance

---
+++ src/Core_java/inheritance/hierachical_inheritance/SuperClass.java
@@ -0,0 +1,13 @@
+package Core_java.inheritance.hierachical_inheritance;
+
+public class SuperClass {
+
+    String car = "Mercedes";
+    int year = 2023;
+
+    void displaySuperClass() {
+
+        System.out.println("My new car " + car + " year " + year);
+
+    }
+}
