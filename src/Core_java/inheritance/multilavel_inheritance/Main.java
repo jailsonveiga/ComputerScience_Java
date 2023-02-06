ae7bc144ff8aca2e56f51eab00141b1fd77468b9
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-02-06 11:51:57 -0500

    Multi Level Inheritance

---
+++ src/Core_java/inheritance/multilavel_inheritance/Main.java
@@ -0,0 +1,12 @@
+package Core_java.inheritance.multilavel_inheritance;
+
+public class Main {
+    public static void main(String[] args) {
+
+        MulLevInhSubClass_1 obj = new MulLevInhSubClass_1();
+        obj.displayMulLevInh();
+        obj.displayMulLevInhSubClass();
+        obj.displayMulLevInhSubClass_1();
+
+    }
+}
