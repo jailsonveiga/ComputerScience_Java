25aec150b2a5bd23dda8e981044368438099bfe9
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-02-06 12:09:24 -0500

    Hierarchical Inheritance

---
+++ src/Core_java/inheritance/hierachical_inheritance/Main.java
@@ -0,0 +1,20 @@
+package Core_java.inheritance.hierachical_inheritance;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        SubClass obj1 = new SubClass();
+        obj1.displaySuperClass();
+        obj1.displaySubClass();
+
+        System.out.println("--------------------");
+
+        SubClass_1 obj = new SubClass_1();
+        obj.displaySuperClass();
+        obj.displaySubClass_1();
+
+
+
+    }
+}
