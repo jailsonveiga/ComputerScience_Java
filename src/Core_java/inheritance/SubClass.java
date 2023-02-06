b5599a85a040a2c271bfdd0a66edb91866ebf073
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-02-05 23:05:43 -0500

    Single Inheritance

---
+++ src/Core_java/inheritance/SubClass.java
@@ -0,0 +1,13 @@
+package Core_java.inheritance;
+
+public class SubClass extends ParentClass{
+
+    String many = "It's been programing for ";
+    int year = 10;
+
+    void displaySubClass() {
+
+        System.out.println(many + year + " years");
+
+    }
+}
