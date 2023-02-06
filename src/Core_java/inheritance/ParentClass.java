36f3ec665b49dd9bf8bed1ecd9fa1aff278fdbf0
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-02-05 23:05:49 -0500

    Single Inheritance

---
+++ src/Core_java/inheritance/ParentClass.java
@@ -0,0 +1,15 @@
+package Core_java.inheritance;
+
+public class ParentClass {
+
+    String programing = "Love Programing";
+    String name = "Jay ";
+
+    void displayParentClass() {
+
+        System.out.println(name + programing);
+
+    }
+
+
+}
