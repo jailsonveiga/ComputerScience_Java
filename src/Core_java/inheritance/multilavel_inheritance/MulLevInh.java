0e3daa446b0a9216f1370c1492318a805056c13d
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-02-06 11:51:38 -0500

    Multi Level Inheritance

---
+++ src/Core_java/inheritance/multilavel_inheritance/MulLevInh.java
@@ -0,0 +1,13 @@
+package Core_java.inheritance.multilavel_inheritance;
+
+public class MulLevInh {
+
+    String name = "Jay";
+    int age = 25;
+
+    void displayMulLevInh() {
+
+        System.out.println("My name is " + name + " and I'm " + age + " years old");
+
+    }
+}
