ee944364bf5bd62d3a83cf972b5de9f910bae122
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-02-06 11:51:51 -0500

    Multi Level Inheritance

---
+++ src/Core_java/inheritance/multilavel_inheritance/MulLevInhSubClass_1.java
@@ -0,0 +1,11 @@
+package Core_java.inheritance.multilavel_inheritance;
+
+public class MulLevInhSubClass_1 extends MulLevInhSubClass{
+
+    String love = "Love to programing";
+    String prefer = " in Java";
+
+    void displayMulLevInhSubClass_1() {
+        System.out.println(love + prefer);
+    }
+}
