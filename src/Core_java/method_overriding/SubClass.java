4bdbd153e89044e3d1aac2e63e67e909920c4765
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-02-06 20:01:51 -0500

    Method Overriding

---
+++ src/Core_java/method_overriding/SubClass.java
@@ -0,0 +1,11 @@
+package Core_java.method_overriding;
+
+public class SubClass extends SuperClass{
+
+    void display() {
+
+        super.display();
+        System.out.println("Sub Class");
+
+    }
+}
