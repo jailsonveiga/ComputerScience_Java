9a864c2cf7c9b154ca4e46047ad89422cd8cbc34
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-02-06 19:55:15 -0500

    Super keyword

---
+++ src/Core_java/super_keyword/SuperClass.java
@@ -0,0 +1,19 @@
+package Core_java.super_keyword;
+
+public class SuperClass {
+
+    int age = 25;
+    String name = "jay";
+
+    SuperClass() {
+
+        System.out.println("Using a Super Keyword");
+
+    }
+
+    void displayParentClass() {
+
+        System.out.println("My name is " + name + " and I'm " + age + " years old");
+
+    }
+}
