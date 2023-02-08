9f1064ee647539ff86fbfddd140d7ea4f716a993
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-02-07 23:00:38 -0500

    Interface, Implements

---
+++ src/Core_java/interface_implementation/SubClass.java
@@ -0,0 +1,16 @@
+package Core_java.interface_implementation;
+
+public class SubClass implements ParentClass, ParentClass_1{
+
+    public void display() {
+
+        System.out.println("Parent Class");
+
+    }
+
+    public void show() {
+
+        System.out.println("Parent Class 1");
+
+    }
+}
