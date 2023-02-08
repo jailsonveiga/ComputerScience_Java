b6e6edb157a45724825b9e4a8c7ca23495703dec
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-02-07 23:00:12 -0500

    Interface, Implements

---
+++ src/Core_java/interface_implementation/Main.java
@@ -0,0 +1,12 @@
+package Core_java.interface_implementation;
+
+public class Main {
+    public static void main(String[] args) {
+
+        SubClass obj = new SubClass();
+
+        obj.display();
+        obj.show();
+
+    }
+}
