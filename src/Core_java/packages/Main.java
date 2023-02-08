ff94137a8640a79ebef3680a5752e6fae62cae35
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-02-08 16:25:46 -0500

    Import Packages

---
+++ src/Core_java/packages/Main.java
@@ -0,0 +1,13 @@
+package Core_java.packages;
+import Core_java.super_keyword.SuperClass;
+
+public class Main {
+    public static void main(String[] args) {
+
+        AClass obj = new AClass();
+        obj.display();
+
+        SuperClass obj1 = new SuperClass();
+        obj1.displayParentClass();
+    }
+}
