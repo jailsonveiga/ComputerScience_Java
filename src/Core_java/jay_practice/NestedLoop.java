c8ea72c90c0ad06b97799dab16b24c6a83e4313f
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-02-09 20:42:30 -0500

    Nested Loop

---
+++ src/Core_java/jay_practice/NestedLoop.java
@@ -0,0 +1,13 @@
+package Core_java.jay_practice;
+
+public class NestedLoop {
+    public static void main(String[] args) {
+        for(int i = 0; i < 2; i++) {
+            for(int j = 0; j < 2; j++) {
+
+                System.out.println(i + " " + j);
+
+            }
+        }
+    }
+}
