11ab8fc8bb1cf80e6219754612d288810434a968
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-02-11 11:35:06 -0500

    Simple Math

---
+++ src/Core_java/jay_practice/SimpleMath.java
@@ -0,0 +1,13 @@
+package Core_java.jay_practice;
+
+public class SimpleMath {
+    public static void main(String[] args) {
+        int nums = 3;
+
+        for (int i = 1; i <= 10; i++) {
+            int results = nums * i;
+
+            System.out.println(nums + " x " + i + " = " + results);
+        }
+    }
+}
