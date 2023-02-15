fecb3da9c07bc096561a28703524ce150c25c6b7
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-02-15 16:20:59 -0500

    Reverse String Using StringBuilder

--- src/Core_java/jay_practice/string_reverse/Using_StringBuilder.java
+++ src/Core_java/jay_practice/string_reverse/Using_StringBuilder.java
@@ -1,4 +1,19 @@
 package Core_java.jay_practice.string_reverse;
 
 public class Using_StringBuilder {
+    public static void main(String[] args) {
+        String str = "I Love To Code";
+
+        StringBuilder newStr = new StringBuilder();
+
+        for(int i = str.length() - 1; i >= 0; i--){
+
+            newStr.append(str.charAt(i));
+
+        }
+
+        String reverseString = newStr.toString();
+
+        System.out.println(reverseString);
+    }
 }
