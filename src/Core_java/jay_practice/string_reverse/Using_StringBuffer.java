6c0f3e3987e26570d5aace605ff7594d68504e0f
Jailson Veiga <jmdaveiga2012@hotmail.com>
2023-02-16 18:52:33 -0500

    Reverse String Using StringBuffer

--- src/Core_java/jay_practice/string_reverse/Using_StringBuffer.java
+++ src/Core_java/jay_practice/string_reverse/Using_StringBuffer.java
@@ -1,4 +1,15 @@
 package Core_java.jay_practice.string_reverse;
 
 public class Using_StringBuffer {
+    public static void main(String[] args) {
+        String str = "I Love To Code";
+
+        StringBuffer newStr = new StringBuffer(str);
+
+        newStr.reverse();
+
+        String reverseString = newStr.toString();
+
+        System.out.println(reverseString);
+    }
 }
